7309427a3b7421e18b87c25cd40b5825e345d729
millionviewsmusic <millionviewsmusic1@gmail.com>
2025-06-05 11:42:12 +0530

    Update HomePage.jsx

--- src/app/pages/HomePage.jsx
+++ src/app/pages/HomePage.jsx
@@ -10,8 +10,7 @@ const HomePage = () => {
     >
       <div className="w-full h-72 sm:h-full md:w-1/2 order-2 md:order-1 flex items-start sm:items-center justify-center p-6 md:p-8 text-center md:text-left">
         <h1 className="text-3xl md:text-5xl text-white font-lora leading-snug">
-          Music That Speaks to the Soul <br />
-          <p className="text-center sm:text-right">Created from the Heart</p>
+          Music That Speaks to the Soul,created from the Heart
         </h1>
       </div>
 
