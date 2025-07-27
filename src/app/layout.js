812de5de9620b1176fcccefccf345451c1e24952
millionviewsmusic <millionviewsmusic1@gmail.com>
2025-07-27 11:43:27 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,6 +31,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+    <meta name="google-adsense-account" content="ca-pub-2482822338880687" />
     <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2482822338880687"
